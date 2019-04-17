b2ae5d1337d74dd16ae0367a40cf262cd7302294
Kirsty Darbyshire <kirsty@loquax.com>
2019-04-17 16:25:36 +0100

    challenge 2

--- Project 21/ViewController.swift
+++ Project 21/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import UserNotifications
 
 class ViewController: UIViewController, UNUserNotificationCenterDelegate     {
+    var timeInterval = 5.0 // default to send notification after 5 seconds
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,7 +48,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate     {
         dateComponents.hour = 10
         dateComponents.minute = 30
         //let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
         
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
@@ -59,7 +60,8 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate     {
         center.delegate = self
         
         let show = UNNotificationAction(identifier: "show", title: "Tell me more", options: .foreground)
-        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [], options: [])
+        let remind = UNNotificationAction(identifier: "remind", title: "Remind Me Later", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show, remind], intentIdentifiers: [], options: [])
         
         center.setNotificationCategories([category])
     }
@@ -71,13 +73,21 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate     {
             
             switch response.actionIdentifier {
             case UNNotificationDefaultActionIdentifier:
-                // the user swiped to unlock
-                print("default identifier")
                 let ac = UIAlertController(title: "default", message: "You clicked on the default identifier for the notification", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
                 present(ac, animated: true)
             case "show":
-                print("the user clicked the button")
                 let ac = UIAlertController(title: "Here's More", message: "Thank you for clicking on the Tell Me More button", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+            case "remind":
+                timeInterval = 30 // 30 secs is much easier to test than 86400
+                let ac = UIAlertController(title: "Reminder", message: "We'll remind you again in \(timeInterval) seconds", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default) {
+                    _ in
+                    self.scheduleLocal()
+                })
+                ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
                 present(ac, animated: true)
             default:
                 break
